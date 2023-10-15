653a00b5937ed38a9645575de8ee97df3de6c94c
ue555 <zerozero53205320@gmail.com>
2023-10-16 01:07:10 +0900

    zenn-editorの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import "zenn-content-css";
+import { useEffect } from "react";
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("zenn-embed-elements"); // 数式をブラウザでレンダリングできるようにします
+  }, []);
+  return <Component {...pageProps} />;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>{" "}
+        // 一部の埋め込みで、高さ調節のためにスクリプトを利用します
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
