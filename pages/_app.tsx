91922740acb4c2d968d0c74b71abeb16b2a58f84
ue555 <zerozero53205320@gmail.com>
2023-10-22 00:20:57 +0900

    headerとfooterの修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app";
 import "../styles/index.css";
 import "zenn-content-css";
 import { useEffect } from "react";
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     import("zenn-embed-elements");
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,14 +2,13 @@ import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import Container from "../../components/container";
 import PostBody from "../../components/post-body";
-import Header from "../../components/header";
 import PostHeader from "../../components/post-header";
 import Layout from "../../components/layout";
 import { getPostBySlug, getAllPosts } from "../../lib/api";
 import PostTitle from "../../components/post-title";
 import Head from "next/head";
 import { CMS_NAME } from "../../lib/constants";
-import markdownToHtml from "../../lib/markdownToHtml";
+import markdownToHtml from "zenn-markdown-html";
 import type PostType from "../../interfaces/post";
 
 type Props = {
@@ -27,7 +26,6 @@ export default function Post({ post, morePosts, preview }: Props) {
   return (
     <Layout preview={preview}>
       <Container>
-        <Header />
         {router.isFallback ? (
           <PostTitle>Loading…</PostTitle>
         ) : (
