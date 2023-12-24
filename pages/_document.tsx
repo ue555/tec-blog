6ebca2895d1dccbc42db4b95fb255197964aac26
ue555 <122501501+ue555@users.noreply.github.com>
2023-12-25 02:43:30 +0900

    Revert "ブログ投稿の日付表示ズレの修正"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script src="https://embed.zenn.studio/js/listen-embed-event.js"></script>{" "}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
